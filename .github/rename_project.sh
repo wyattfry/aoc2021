6e16142d12edac04300d97a0663146dee3369360
wyattfry <wyattfry@users.noreply.github.com>
2022-03-21 17:20:01 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="wyattfry"
+original_name="aoc2021"
+original_urlname="aoc2021"
+original_description="Awesome aoc2021 created by wyattfry"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv aoc2021 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- aoc2021/base.py
+++ aoc2021/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+aoc2021 base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the aoc2021 project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
